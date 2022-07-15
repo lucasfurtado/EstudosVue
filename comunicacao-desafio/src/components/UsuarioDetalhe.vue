e657abb7dfe535c8f0252e8c291a9fce61e347d1
Lucas Furtado Ananias de Sant'Anna <lucas.santanna@direcional.com.br>
2022-07-15 14:28:27 -0300

    comunicacao entre componentes

---
+++ comunicacao-desafio/src/components/UsuarioDetalhe.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="usuario-detalhe">
+        <h2 v-if="!this.usuario">Usuário não selecionado!</h2>
+        <div v-else>
+            <h2>ID: {{ this.usuario.id }}</h2>
+            <h2>Nome: {{ this.usuario.nome }}</h2>
+            <h2>Idade: {{ this.usuario.idade }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['usuario'],
+    methods:{
+        teste(){
+            return true;
+        }
+    }
+}
+</script>
+
+<style>
+    .usuario-detalhe {
+        flex: 1;
+        border: 1px solid #CCC;
+    }
+</style>
---
+++ comunicacao-desafio/src/components/UsuarioLista.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="usuario-lista">
+        <table>
+            <thead>
+                <tr>
+                    <th>#ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="usuario in usuarios" :key="usuario.id" @click="eventousuarioSelecionado(usuario)">
+                    <td>{{ usuario.id }}</td>
+                    <td>{{ usuario.nome }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: { usuarios: Array },
+    data(){
+        return{
+            usuarioSelected : null
+        }
+
+    },
+    methods:{
+        eventousuarioSelecionado(user){
+            this.usuarioSelected = user
+            this.$emit('onUserSelected',this.usuarioSelected)
+        }   
+    }
+}
+</script>
+
+<style scoped>
+    .usuario-lista {
+        flex: 1;
+        display: flex;
+    }
+
+    table {
+        flex: 1;
+        border-spacing: 0px;
+        border-collapse: separate;
+        border-top: 1px solid #CCC;
+    }
+
+
+    th, td {
+        padding: 15px;
+        border-bottom: 1px solid #CCC;
+    }
+
+    tbody tr:hover {
+        background: rgba(0, 0, 0, 0.1);
+    }
+</style>
